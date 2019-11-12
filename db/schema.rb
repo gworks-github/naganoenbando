f2f429c33f494fcf90990384d5d76347e3a75e03
kottu55 <kotsuboon@gmail.com>
2019-11-12 10:24:13 +0000

    added bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_11_12_065335) do
 
-  create_table "items", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "artist_id", null: false
-    t.boolean "format", null: false
-    t.string "jacket_image"
-    t.integer "genre_id", null: false
-    t.integer "label_id", null: false
-    t.integer "quantity", default: 1, null: false
-    t.date "release_date", null: false
-    t.boolean "is_selling", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "prices"
-  end
-    
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "item_id"
@@ -60,6 +44,21 @@ ActiveRecord::Schema.define(version: 2019_11_12_065335) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "artist_id", null: false
+    t.boolean "format", null: false
+    t.string "jacket_image"
+    t.integer "genre_id", null: false
+    t.integer "label_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.date "release_date", null: false
+    t.boolean "is_selling", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "prices"
+  end
+
   create_table "order_details", force: :cascade do |t|
     t.integer "order_id"
     t.integer "item_id"
@@ -88,15 +87,6 @@ ActiveRecord::Schema.define(version: 2019_11_12_065335) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.integer "artist_id", null: false
-    t.boolean "format", null: false
-    t.string "jacket_image"
-    t.integer "genre_id", null: false
-    t.integer "label_id", null: false
-    t.integer "quantity", default: 1, null: false
-    t.date "release_date", null: false
-    t.boolean "is_selling", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
