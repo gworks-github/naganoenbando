d1f109e44a0847a31e53b20f57f3ce349ff2a6df
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-11-12 15:20:25 +0900

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -20,18 +20,6 @@ ActiveRecord::Schema.define(version: 2019_11_12_033829) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-  
   create_table "customers", force: :cascade do |t|
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -53,7 +41,19 @@ ActiveRecord::Schema.define(version: 2019_11_12_033829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: trueend
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
-  
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
