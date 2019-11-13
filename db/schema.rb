418e02f9f514965e2efa52c91705fc1f0d9f5dbc
kottu55 <kotsuboon@gmail.com>
2019-11-13 10:59:42 +0000

    cartindex created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_105433) do
+ActiveRecord::Schema.define(version: 2019_11_13_072202) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "customer_id"
