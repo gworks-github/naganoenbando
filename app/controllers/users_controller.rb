b44632b23cd63e0f0064f785a59786bef492ce38
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-11-17 14:15:51 +0900

    Fix no_direct_URL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
-  def show
+
+  before_action :correct_customer, only: [:show, :destroy]
+  def correct_customer
     @customer   = Customer.find(params[:id])
+    @cust_check = current_customer
+    unless @customer == @cust_check
+      redirect_to root_path
+    end
+  end
+
+  def show
     @deliveries = @customer.deliveries
     @delivery   = @customer.deliveries.new
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   def destroy
   end
+
 end
