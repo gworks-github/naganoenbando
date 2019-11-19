47642192e9763d0fc3165a09337c52bbe9274184
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-11-19 16:12:48 +0900

    Add Admin_customers_edit_feateru

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,9 +10,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+  	@customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
   end
 
   def destroy
@@ -21,6 +25,11 @@ class Admin::CustomersController < ApplicationController
   def search
     @customers = Customer.all.cust_search(params)
     render :index
+  end
+
+  private
+  def customer_params
+  	params.require(:customer).permit(:first_name, :last_name, :first_furigana, :last_furigana, :post_code, :address, :phone_number, :email)
   end
 
 end
