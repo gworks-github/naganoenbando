116e2a05e8a25c7547da855e45147dda73d9473c
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-11-19 12:30:16 +0900

    Fix Admins's customer index&show_views

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,9 +1,12 @@
 class Admin::CustomersController < ApplicationController
 
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
+    @deliveries = @customer.deliveries
   end
 
   def edit
