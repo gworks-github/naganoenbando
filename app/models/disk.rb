3c83a4d653ea8a8ed73bf572ee426b9c16169345
mizu19961203 <mizu19961203@gmail.com>
2019-11-20 07:33:49 +0000

    Added_item_form

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -1,4 +1,5 @@
 class Disk < ApplicationRecord
 		belongs_to :item
-		has_many :tracks, dependent: :destroy
+		has_many :tracks, dependent: :destroy, inverse_of: :disk
+		accepts_nested_attributes_for :tracks, allow_destroy: true
 end
