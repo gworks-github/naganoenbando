b9166d90c7655a895c9b34146f598ab5743c4638
kottu55 <kotsuboon@gmail.com>
2019-11-24 02:31:28 +0000

    turborinks added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require cocoon
-
+//= require turbolinks
 //= require jquery-ui/widgets/datepicker
 //= require jquery-ui/i18n/datepicker-ja
 //= require bootstrap-sprockets
